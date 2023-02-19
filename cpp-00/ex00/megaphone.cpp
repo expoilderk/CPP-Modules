4276bfe8cc9db058cd835e68ea78efc7f8ea71ef
expoilderk <falecommarlonreis@gmail.com>
2023-02-19 13:03:57 -0300

    ex00 finalizado

--- cpp-00/ex00/megaphone.cpp
+++ cpp-00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: mreis-me <mreis-me@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 23:52:13 by mreis-me          #+#    #+#             */
-/*   Updated: 2023/01/31 01:21:11 by mreis-me         ###   ########.fr       */
+/*   Updated: 2023/02/19 12:59:26 by mreis-me         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,16 @@ int main(int argc, char **argv)
 		return 0;
 	}
 
-	for (size_t i = 1; i < argc; i++)
+	for (int i = 1; i < argc; i++)
 	{
-		for (size_t j = 0; j < strlen(argv[i]); j++)
+		for (int j = 0; j < (int)strlen(argv[i]); j++)
 		{
 			if ((argv[i][j] == '\"') || (argv[i][j] == '\''))
 				j++;
-			std::cout << (char)toupper(argv[i][j]);
+			else
+				std::cout << (char)toupper(argv[i][j]);
 		}
-		if (argv[i+1] != NULL) // Falta arrumar os espaços
-			std::cout << " ";
-		if (argv[i][0] == ' ')
-			std::cout << " ";
 	}
-	std::cout<<"\n";
+	std::cout << "\n";
 	return 0;
 }
