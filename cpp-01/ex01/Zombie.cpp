4df1b32820d08caa72496554830a8356f3e9b880
expoilderk <falecommarlonreis@gmail.com>
2023-02-21 14:24:10 -0300

    cpp-01 ex01 finish

---
+++ cpp-01/ex01/Zombie.cpp
@@ -0,0 +1,17 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie() {}
+
+Zombie::~Zombie()
+{
+    std::cout << _name << ": has been destroyed!" << std::endl;
+}
+
+void Zombie::announce(void)
+{
+    std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+void Zombie::setName(std::string name) {
+    this->_name = name;
+}
---
+++ cpp-01/ex01/main.cpp
@@ -0,0 +1,19 @@
+#include "Zombie.hpp"
+
+Zombie* zombieHorde( int N, std::string name );
+
+int main()
+{
+	int size = 5;
+
+	Zombie* horde = zombieHorde(size, "ZombieHorde");
+
+	for (int i = 0; i < size; i++)
+	{
+		horde[i].announce();
+	}
+
+	delete[] horde;
+
+	return 0;
+}
