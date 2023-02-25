e1f2c14070441a0cdcbc1846bcec33111dbfdba2
expoilderk <falecommarlonreis@gmail.com>
2023-02-25 01:56:16 -0300

    cpp-03 ex01 finish

---
+++ cpp-03/ex01/ClapTrap.hpp
@@ -0,0 +1,37 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+#include <string>
+
+class ClapTrap	{
+	protected:
+		std::string _name;
+		int _hitPoints;
+		int _energyPoints;
+		int _attackDamage;
+		ClapTrap();
+	public:
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap& other);
+		ClapTrap& operator=(const ClapTrap& other);
+		~ClapTrap();
+
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+
+		/* -- Setters -- */
+		void setName(std::string const raw);
+		void setHitPoints(int const raw);
+		void setEnergyPoints(int const raw);
+		void setAttackDamage(int const raw);
+
+		/* -- Getters -- */
+		std::string getName(void) const;
+		int getHitPoints(void) const;
+		int getEnergyPoints(void) const;
+		int getAttackDamage(void) const;
+};
+
+#endif // CLAPTRAP_HPP
---
+++ cpp-03/ex01/main.cpp
@@ -0,0 +1,47 @@
+#include "ScavTrap.hpp"
+
+int main()
+{
+	ScavTrap a("Expoilderk");
+	ScavTrap b("Josh");
+
+	std::cout << "\n";
+	std::cout << "Name: " << a.getName() << " | Attack Damage: " << a.getAttackDamage(); 
+	std::cout << " | Hits: " << a.getHitPoints() << " | Energy: " << a.getEnergyPoints() << std::endl;
+	
+	std::cout << "Name: " << b.getName() << " | Attack Damage: " << b.getAttackDamage(); 
+	std::cout << " | Hits: " << b.getHitPoints() << " | Energy: " << b.getEnergyPoints() << std::endl;
+
+	std::cout << "\n";
+	a.attack("Josh");
+	b.attack("Expoilderk");
+
+	std::cout << "\n";
+	b.takeDamage(a.getAttackDamage());
+	a.takeDamage(b.getAttackDamage());
+
+	std::cout << "\n";
+	std::cout << "Name: " << a.getName() << " | Attack Damage: " << a.getAttackDamage(); 
+	std::cout << " | Hits: " << a.getHitPoints() << " | Energy: " << a.getEnergyPoints() << std::endl;
+	
+	std::cout << "Name: " << b.getName() << " | Attack Damage: " << b.getAttackDamage(); 
+	std::cout << " | Hits: " << b.getHitPoints() << " | Energy: " << b.getEnergyPoints() << std::endl;
+
+	std::cout << "\n";
+	a.beRepaired(2);
+	b.beRepaired(2);
+	
+	std::cout << "\n";
+	a.guardGate();
+	b.guardGate();
+
+	std::cout << "\n";
+	std::cout << "Name: " << a.getName() << " | Attack Damage: " << a.getAttackDamage(); 
+	std::cout << " | Hits: " << a.getHitPoints() << " | Energy: " << a.getEnergyPoints() << std::endl;
+	
+	std::cout << "Name: " << b.getName() << " | Attack Damage: " << b.getAttackDamage(); 
+	std::cout << " | Hits: " << b.getHitPoints() << " | Energy: " << b.getEnergyPoints() << std::endl;
+	std::cout << "\n";
+
+	return 0;
+}
